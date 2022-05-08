373de25136df305ff074446b033864887969f79f
oliUmbi <oliver.umbricht@hispeed.ch>
2022-05-08 02:12:29 +0200

    Log ex

--- src/main/java/ch/teachu/teachuapi/errorhandling/ExceptionHandling.java
+++ src/main/java/ch/teachu/teachuapi/errorhandling/ExceptionHandling.java
@@ -1,6 +1,8 @@
 package ch.teachu.teachuapi.errorhandling;
 
 import ch.teachu.teachuapi.dtos.ErrorDTO;
+import ch.teachu.teachuapi.enums.LogLevel;
+import ch.teachu.teachuapi.util.LogUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +17,7 @@ public class ExceptionHandling {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorDTO exceptionHandler(Exception exception) {
+        LogUtil.log(exception.getMessage(), exception.getStackTrace()[0].getClassName(), LogLevel.ERROR);
         return new ErrorDTO(exception.getMessage());
     }
 
@@ -22,6 +25,7 @@ public class ExceptionHandling {
     @ExceptionHandler(InvalidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDTO exceptionHandler(InvalidException exception) {
+        LogUtil.log(exception.getMessage(), exception.getStackTrace()[0].getClassName(), LogLevel.ERROR);
         return new ErrorDTO(exception.getMessage());
     }
 
@@ -29,6 +33,7 @@ public class ExceptionHandling {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDTO exceptionHandler(NotFoundException exception) {
+        LogUtil.log(exception.getMessage(), exception.getStackTrace()[0].getClassName(), LogLevel.WARN);
         return new ErrorDTO(exception.getMessage());
     }
 
@@ -36,6 +41,7 @@ public class ExceptionHandling {
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorDTO exceptionHandler(UnauthorizedException exception) {
+        LogUtil.log(exception.getMessage(), exception.getStackTrace()[0].getClassName(), LogLevel.INFO);
         return new ErrorDTO(exception.getMessage());
     }
 }
